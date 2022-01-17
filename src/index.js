5a338a69209cbec617b4c9eb8abbe88bf1c71a2b
biswajit samal <biswajit_samal@persistent.com>
2022-01-17 15:22:57 +0530

    webpack configured for babel and basic react component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home';
+
+ReactDOM.render(
+  <Home />,
+  document.getElementById('app')
+);
